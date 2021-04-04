9640c03f11ca604911a166d71280b99efa8e3348
pavithra <pavinuthana@gmail.com>
2021-04-04 12:43:54 +0530

    Intial commit

--- Python.py
+++ Python.py
@@ -4,13 +4,13 @@
 def changeme( mylist ):
    "This changes a passed list into this function"
    mylist.append([1,2,3,4]);
-   print "Values inside the function: ", mylist
+   print ("Values inside the function: ", mylist)
    return
 
-# Now you can call changeme function
+# Now you can call changeme functio
 mylist = [10,20,30];
 changeme( mylist );
-print "Values outside the function: ", mylist
+print ("Values outside the function: ", mylist)
 
 
 
